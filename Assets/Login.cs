5739bc8b29c0eac266c6acec3d9e4c932a1610e4
David <57778175+dchen046@users.noreply.github.com>
2020-11-08 19:39:11 -0500

    initial

---
+++ Assets/Login.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Button loginButton;
+    public Button regButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+       loginButton.onClick.AddListener(() => 
+       {
+            /**
+             * When clicked make sure both inputs are there
+             * Then check the database to see if it exists
+             * If exists then go to next scene
+             * else error message
+             */
+            print("login pressed");
+       });
+
+       regButton.onClick.AddListener(() =>
+       {
+            /**
+             * Make sure both inputs are there
+             * Then check if username is already registered 
+             * If its not then register the person and message
+             * Else error message popup
+             */
+            print("register pressed");
+       });
+    }
+    
+
+}
---
+++ Assets/Web.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class will be used to establish the connection and have some functions to interact with database
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
