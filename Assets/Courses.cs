94032834a6d76b930e349289c8c513edd21eef77
David <57778175+dchen046@users.noreply.github.com>
2020-11-15 21:32:00 -0500

    added course and topic scenes

---
+++ Assets/Courses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Courses : MonoBehaviour
+{
+    public Button biochemButton;
+    public Button LogoutButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        biochemButton.onClick.AddListener(() =>
+        {
+            // TODO
+            print("biochem pressed");
+            SceneManager.LoadScene("TopicScene");
+        });
+
+        LogoutButton.onClick.AddListener(() =>
+        {
+            // TODO
+            print("logout pressed");
+            SceneManager.LoadScene("LoginScene");
+        });
+
+
+    }
+
+}
---
+++ Assets/Topics.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Topics : MonoBehaviour
+{
+    public Button topic1;
+    public Button topic2;
+    public Button topic3;
+    public Button LogoutButton;
+    public Button backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        setLogoutButton();
+        setBackButton();
+        
+    }
+
+    private void setLogoutButton()
+    {
+        LogoutButton.onClick.AddListener(() =>
+        {
+            // TODO
+            print("logout pressed");
+            SceneManager.LoadScene("LoginScene");
+        });
+    }
+
+    private void setBackButton()
+    {
+        backButton.onClick.AddListener(() =>
+        {
+
+            /**
+             * Make sure both inputs are there
+             * Then check if username is already registered 
+             * If its not then register the person and message
+             * Else error message popup
+             */
+            print("back pressed");
+            SceneManager.LoadScene("CourseScene");
+        });
+    }
+
+    private void setTopicButtons()
+    {
+
+    }
+}
