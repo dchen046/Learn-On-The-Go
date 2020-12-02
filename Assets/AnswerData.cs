5b09ffedffb1ce8b6300584e5a25fb99dbd67116
crystallized0809 <35966507+crystallized0809@users.noreply.github.com>
2020-12-01 20:22:25 -0500

    Add files via upload
    Scripts for Fill In Blank Game

---
+++ Assets/AnswerData.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerData : MonoBehaviour {
+    [Header("UI Elements")]
+    [SerializeField] TextMeshProUGUI infoTextObject = null;
+    [SerializeField] Image toggle = null;
+
+    [Header("Textures")]
+    [SerializeField] Sprite uncheckedToggle = null;
+    [SerializeField] Sprite checkedToggle = null;
+
+    [Header("References")]
+    [SerializeField] GameEvents events = null;
+
+    private RectTransform rect = null;
+    public RectTransform Rect
+    {
+        get
+        {
+            if (rect == null)
+            {
+                rect = GetComponent<RectTransform>() ?? gameObject.AddComponent<RectTransform>();
+            }
+            return rect;
+        }
+    }
+    private int answerIndex = -1;
+    public int AnswerIndex { get { return answerIndex; } }
+    private bool Checked = false;
+
+    
+    public void UpdateData (string info, int index)
+    {
+        infoTextObject.text = info;
+        answerIndex = index;
+    }
+   
+    public void Reset ()
+    {
+        Checked = false;
+        UpdateUI();
+    }
+    
+    public void SwitchState ()
+    {
+        Checked = !Checked;
+        UpdateUI();
+
+        if (events.UpdateQuestionAnswer != null)
+        {
+            events.UpdateQuestionAnswer(this);
+        }
+    }
+   
+    void UpdateUI ()
+    {
+        if (toggle == null) return;
+
+        toggle.sprite = (Checked) ? checkedToggle : uncheckedToggle;
+    }
+}
