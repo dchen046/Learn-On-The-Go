f22534179c9416dfea914df26a612ea96940c6e6
David <57778175+dchen046@users.noreply.github.com>
2020-12-01 22:18:20 -0500

    fixed button actions

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -22,14 +22,8 @@ public class Login : MonoBehaviour
     {
         loginButton.onClick.AddListener(() =>
         {
-            /**
-             * When clicked make sure both inputs are there
-             * Then check the database to see if it exists
-             * If exists then go to next scene
-             * else error message
-             */
-            StartCoroutine(Main.Instance.Web.Login(usernameInput.text, passwordInput.text));
             print("login pressed");
+            StartCoroutine(Main.Instance.Web.Login(usernameInput.text, passwordInput.text));
         });
     }
 
@@ -37,13 +31,6 @@ public class Login : MonoBehaviour
     {
         regButton.onClick.AddListener(() =>
         {
-
-            /**
-             * Make sure both inputs are there
-             * Then check if username is already registered 
-             * If its not then register the person and message
-             * Else error message popup
-             */
             print("register pressed");
             SceneManager.LoadScene("RegisterScene");
         });
