c42fdf9b4de351b5c202fc633d140f904cdba127
Shakthi Shrree Ganta Gameshwaran <49251026+areum-fanny@users.noreply.github.com>
2020-12-06 14:27:45 -0500

    Add files via upload

---
+++ Assets/Scripts/CrosswordMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+public class CrosswordMainMenu : MonoBehaviour
+{
+    void Start()
+    {  
+    }
+    public void btnTest(Button btn) {
+        if(btn.name == "BioChemistry"){
+            SceneManager.LoadScene("Crossword");
+        }
+        else if(btn.name == "Biology"){
+            SceneManager.LoadScene("Crossword_1");
+        }
+        else if(btn.name == "Mainmenu") {
+            SceneManager.LoadScene("TopicScene");
+        }
+    }
+    void Update()
+    {
+    }
+}
